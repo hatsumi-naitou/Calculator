e35aab396c395580f8986de25486f9b7067ac724
h.naitou <hatsumi.naitou@gmail.com>
2012-08-02 22:34:10 +0900

    0802計算機

--- src/sample/application/calculator/CaluculatorActivity.java
+++ src/sample/application/calculator/CaluculatorActivity.java
@@ -9,6 +9,9 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class CaluculatorActivity extends Activity {
+	
+	int intadd1, intadd2;
+	
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,12 +26,42 @@ public class CaluculatorActivity extends Activity {
     }
     
     public void numKeyOnClick(View v){
-    	Button button = (Button)v;          //Button型を使いたいのでキャストを使う
+    	/*
+    	 * いろいろ考えてみた。
+    	 * 
+    	 * Button button = (Button)v;          //Button型を使いたいのでキャストを使う
     										//みているインスタンスは同じ。型を変えただけ。
     	Log.d("[buttonのtext]", button.getText().toString()); //とりあえず何か表示させたいよ、というとき。
 
-    	/*TextView et = (TextView)this.findViewById("@+id/displayPanel");
-    	SharedPreferences pref = this.
-    	et.setText(button.text);            //7を取り出す方法。表示領域に入れるという方法。*/
-    }
+    	TextView tv = (TextView)this.findViewById(R.id.displayPanel);
+    	tv.
+    	//String st = tv.getText().toString();
+    	//tv.setText(st);            //7を取り出す方法。表示領域に入れるという方法。
+    	//et.setSelection(st);
+    	//String stt = (String)((button)v).getText(); 
+    	 * なかなか正解にたどり着きません。*/
+    	
+    	/*正解は、、、*/
+    	//v.getText();   ←getTextを使いたのでドットでつなげてみたけど、getTextはViewクラスにはないというエラー。
+    	Button button = (Button)v;     //button型で使いたい。キャストしよう。
+    	Log.d("[buttonのtext]", button.getText().toString());  //buttonについてgetText（テキストを引っ張ってきたもの）をtoString（ストリング型に）する。
+    	TextView tv = (TextView)this.findViewById(R.id.displayPanel);   //（R.id.displayPnel）はTextView型だが、findViewByIdメソッドとしてはView型でかえってくるため、TextViewにキャストしてからtvへ代入。
+    	/*ログを使って、TextView型のオブジェクトかどうかを確認してみる。*/
+    	//Log.d("[tvのインスタンスかどうかを確認]", "tv.text: " + tv.getText().toString()); 
+    	//get~ゲッター　何か値を持ってくる。 set~セッター　フィールドに何か値を持たせる　という命名の慣習がある。
+    	tv.setText(tv.getText().toString() + button.getText().toString());
+    	}
+    
+    //加算するときにはどうしたらよいか？
+	    public void addKeyOnClick(View v){
+	    	Button button = (Button)v;
+	    	TextView tv = (TextView)this.findViewById(R.id.displayPanel);
+	    	String add = (tv.getText().toString());
+	    	Integer intadd1 = Integer.parseInt(add);
+	    	tv.setText("0");
+	    	tv.setText(tv.getText().toString());
+	    	Integer intadd2 = Integer.parseInt(tv.getText().toString());
+	    	//tv.setText(intadd1 + intadd2);
+	    }  
+    			
 }
